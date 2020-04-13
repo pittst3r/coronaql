8930e451287f23c3fe2587d36a13fc3f15b81033
Robbie Pitts <me@robbie-pitts.com>
2020-04-13 12:14:13 -0400

    💥 remove broken out types for now

--- src/schema/types/Node.ts
+++
@@ -1,9 +0,0 @@
-import { interfaceType } from "@nexus/schema";
-
-export const Node = interfaceType({
-  name: "Node",
-  definition(t) {
-    t.id("id", { description: "Unique identifier for the resource" });
-    t.resolveType(() => null);
-  },
-});
